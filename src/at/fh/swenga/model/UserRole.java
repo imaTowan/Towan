73bdd37e2390b3f22615605660fbffd519b0d736
ellifellner <fellnerelisabeth@gmx.net>
2017-05-27 18:30:14 +0200

    Registration in Progress

---
+++ src/at/fh/swenga/model/UserRole.java
@@ -0,0 +1,54 @@
+package at.fh.swenga.model;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import javax.persistence.GenerationType;
+
+@Entity
+@Table(name = "user_roles")
+public class UserRole implements java.io.Serializable {
+	private static final long serialVersionUID = 8098173157518993615L;
+	
+	private Integer userRoleId;
+	private UserModel user;
+	private String role;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "user_role_id", unique = true, nullable = false)
+	public Integer getUserRoleId() {
+		return userRoleId;
+	}
+
+	public void setUserRoleId(Integer userRoleId) {
+		this.userRoleId = userRoleId;
+	}
+/*
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "username", nullable = false)
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+*/
+	@Column(name = "role", nullable = false, length = 45)
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+}
