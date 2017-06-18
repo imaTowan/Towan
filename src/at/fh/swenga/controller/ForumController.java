7608bcd78342046f9669a5701d661c6a7ce1f270
ellifellner <fellnerelisabeth@gmx.net>
2017-06-18 19:59:36 +0200

    Forum

--- src/at/fh/swenga/controller/ForumController.java
+++ src/at/fh/swenga/controller/ForumController.java
@@ -2,15 +2,18 @@ package at.fh.swenga.controller;
 
 import java.util.List;
 
+import org.hibernate.Hibernate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import at.fh.swenga.dao.EntryRepository;
 import at.fh.swenga.dao.ForumRepository;
+import at.fh.swenga.dao.SubforumRepository;
+import at.fh.swenga.model.EntryModel;
 import at.fh.swenga.model.ForumModel;
 import at.fh.swenga.model.SubforumModel;
-import at.fh.swenga.dao.SubforumRepository;
 
 @Controller
 public class ForumController {
@@ -21,6 +24,9 @@ public class ForumController {
 	
 	@Autowired
 	SubforumRepository subforumRepository;
+	
+	@Autowired
+	EntryRepository entryRepository;
 	
 	@RequestMapping(value = "/forum")
 	public String showForum(Model model) {
@@ -33,6 +39,14 @@ public class ForumController {
 	public String showSubforum(Model model) {
 		List<SubforumModel> forums = subforumRepository.findAll();
 		model.addAttribute("forums", forums);
-		return "forum";
+		return "forum/subforum";
 	}
+	
+	@RequestMapping(value = "/forum/entry")
+	public String showEntries(Model model) {
+		List<EntryModel> forums = entryRepository.findAll();
+		model.addAttribute("forums", forums);
+		return "forum/subforum";
+	}
+	
 }
--- src/at/fh/swenga/model/SubforumModel.java
+++ src/at/fh/swenga/model/SubforumModel.java
@@ -4,12 +4,17 @@ import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 
 @Entity
 @Table(name = "SUBFORUM")
@@ -32,7 +37,9 @@ public class SubforumModel {
 	@ManyToOne
 	private ForumModel forum;
 	
-	@OneToMany
+	
+	@OneToMany(fetch=FetchType.EAGER)
+	@Fetch(value = FetchMode.SUBSELECT)
 	private List<EntryModel> entries;
 	
 	
