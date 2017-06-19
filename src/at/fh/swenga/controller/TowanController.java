378f78227c7e35d238344ad80a52819ccf2c4b06
Martin Haring <martin.haring2@edu.fh-joanneum.at>
2017-06-19 16:43:47 +0200

    Game optimization and lost efforts on usability of the app

--- src/at/fh/swenga/controller/TowanController.java
+++ src/at/fh/swenga/controller/TowanController.java
@@ -1,11 +1,18 @@
 package at.fh.swenga.controller;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -56,6 +63,20 @@ public class TowanController {
 	@RequestMapping(value = "/game", method = RequestMethod.GET)
 	public String showGame() {
 		return "game";
+	}
+	
+	@RequestMapping(value = "/game2")
+	public String handleDownload(HttpServletResponse response) throws ServletException, IOException {
+		OutputStream out = response.getOutputStream();
+		FileInputStream in = new FileInputStream("Towan/src/lib/natives_win/lwjgl64.dll");
+		byte[] buffer = new byte[4096];
+		int length;
+		while ((length = in.read(buffer)) > 0){
+		    out.write(buffer, 0, length);
+		}
+		in.close();
+		out.flush();
+		return "game2";
 	}
 	
 	@RequestMapping(value = "/towanGame")
